4d10fbb084bef420322f9a2a108f9b24005f09fa
madeline2909 <63311619+madeline2909@users.noreply.github.com>
2022-05-14 02:23:50 -0400

    bye bye header 2

--- dev/js/main-nav/burger-animation.js
+++ dev/js/main-nav/burger-animation.js
@@ -12,10 +12,6 @@ burgerAnimationTimeline
                     .to("#header-1-line-3", {duration:burgerAnimationSpeed, x:"6px", alpha:0}, "createX")
                     .to("#header-1-line-2", {duration:burgerAnimationSpeed, x:"6px"}, "createX")
                     .to("#header-1-line-1",{duration: burgerAnimationSpeed, x:"6px", alpha:0},"createX")
-                    .to("#header-2-line-3", {duration:burgerAnimationSpeed, x:"6px", alpha:0}, "createX")
-                    .to("#header-2-line-2", {duration:burgerAnimationSpeed, x:"6px"}, "createX")
-                    .to("#header-2-line-1",{duration: burgerAnimationSpeed, x:"6px", alpha:0},"createX")
-                    .to("#header-2-line-4",{duration: burgerAnimationSpeed, x:"6px", opacity:100}, "arrowUp")
                     .to("#header-1-line-4",{duration: burgerAnimationSpeed, x:"6px", opacity:100}, "arrowUp")
                     .addPause()
 
